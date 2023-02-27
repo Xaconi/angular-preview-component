360e5fa908a4ca12c508726a0f9d027efb3682b4
Nicolás Joel Giacconi Fernández <xaco.89@gmail.com>
2023-02-27 23:32:46 +0100

    Add input and output tests

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'VSCode Webview Angular';
+
+  public alert(text: string): void {
+    this.title = text;
+  }
 }
