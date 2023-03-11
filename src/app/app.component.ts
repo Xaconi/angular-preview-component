c29d7aca392bad1253bc20348094398f684f7f95
Nicolás Joel Giacconi Fernández <xaco.89@gmail.com>
2023-03-11 13:16:09 +0100

    Add boolean vars modificators

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,16 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit{
   public componentPropsString: Array<string> = [];
+  public componentPropsBoolean: Array<string> = [];
 
 /* @APComponentProps */ public componentProps;
 
   ngOnInit() {
     Object.keys(this.componentProps).forEach(key => {
       if(typeof this.componentProps[key] === 'string') this.componentPropsString.push(key);
+      if(typeof this.componentProps[key] === 'boolean') this.componentPropsBoolean.push(key);
     })
   }
 
   public changePropString(text: string, componentStringKey: string): void {
     this.componentProps[componentStringKey] = text;
+  }
+
+  public changePropBoolean(value: string, componentBooleanKey: string): void {
+    const booleanValue: boolean = value === 'true';
+    this.componentProps[componentBooleanKey] = booleanValue;
   }
 }
